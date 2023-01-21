c05854de071f96c28e4b2af4d59ad071ee2d1238
Ayush Kavad <ayushkavad@gmail.com>
2023-01-21 17:20:35 +0530

    remove img bg

--- src/component/logo/logo.component.jsx
+++ src/component/logo/logo.component.jsx
@@ -1,5 +1,7 @@
+import "./logo.styles.scss";
+
 const Logo = ({ logo }) => {
-  return <img src={logo} width="60" alt="" />;
+  return <img className="logo" src={logo} width="60" alt="" />;
 };
 
 export default Logo;
