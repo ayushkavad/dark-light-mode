503d72c3a5f29437da95c1f2aff83eb9fbe0432e
Ayush Kavad <ayushkavad@gmail.com>
2023-01-21 15:27:53 +0530

    use reducers

--- src/contaxt/mode.contaxt.jsx
+++ src/contaxt/mode.contaxt.jsx
@@ -1,13 +1,40 @@
-import { useState } from "react";
-import { createContext } from "react";
+import { useState, createContext, useReducer } from "react";
 
 export const ModeContaxt = createContext({
   currentMode: false,
   setCurrentMode: () => {},
 });
 
+const ACTION_TYPE = {
+  SET_CURRENT_MODD: "SET_CURRENT_MODE",
+};
+
+const INIT_STATE = {
+  currentMode: false,
+};
+
+const modeReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case ACTION_TYPE.SET_CURRENT_MODD:
+      return {
+        ...state,
+        currentMode: payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
 export const ModeProvider = ({ children }) => {
-  const [currentMode, setCurrentMode] = useState(false);
+  //   const [currentMode, setCurrentMode] = useState(false);
+  const [{ currentMode }, dispatch] = useReducer(modeReducer, INIT_STATE);
+
+  const setCurrentMode = (bool) =>
+    dispatch({ type: ACTION_TYPE.SET_CURRENT_MODD, payload: bool });
+
   const value = { currentMode, setCurrentMode };
 
   return <ModeContaxt.Provider value={value}>{children}</ModeContaxt.Provider>;
